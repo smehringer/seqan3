aff7e362db39ec6da8295e83c10d19d818a0d99f
smehringer <svenja.mehringer@fu-berlin.de>
2020-08-12 15:07:04 +0200

    [MISC] Make scoring configuration usable (fixes seqan/product_backlog#185).

--- test/unit/alignment/multiple/align_multiple_test.cpp
+++ test/unit/alignment/multiple/align_multiple_test.cpp
@@ -11,6 +11,8 @@
 #include <vector>
 
 #include <seqan3/alignment/configuration/align_config_gap.hpp>
+#include <seqan3/alignment/configuration/align_config_scoring.hpp>
+#include <seqan3/alignment/scoring/aminoacid_scoring_scheme.hpp>
 #include <seqan3/alignment/multiple/align_multiple.hpp>
 #include <seqan3/alignment/multiple/detail.hpp>
 #include <seqan3/alphabet/aminoacid/aa27.hpp>
@@ -26,6 +28,31 @@ using seqan3::operator""_dna4;
 using seqan3::operator""_dna5;
 using seqan3::operator""_rna5;
 using seqan3::operator""_rna4;
+
+TEST(detail_initialise_scoring_scheme, no_scoring_configuration)
+{
+    // no scoring information
+    seqan3::configuration config = seqan3::align_cfg::gap{seqan3::gap_scheme{seqan3::gap_score{-2},
+                                                                             seqan3::gap_open_score{-8}}};
+    auto msaOpt = seqan3::detail::initialise_scoring_scheme<seqan::Dna>(config);
+
+    EXPECT_TRUE((std::same_as<decltype(msaOpt.sc), seqan::Score<int>>));
+}
+
+TEST(detail_initialise_scoring_scheme, blosum62)
+{
+    // no scoring information
+    seqan3::aminoacid_scoring_scheme scheme{seqan3::aminoacid_similarity_matrix::BLOSUM62};
+    seqan3::configuration config = seqan3::align_cfg::scoring{scheme};
+    auto msaOpt = seqan3::detail::initialise_scoring_scheme<seqan::AminoAcid>(config);
+
+    // compare matrix size and abort test if not equal so the value comparison does not segfault
+    ASSERT_EQ(static_cast<size_t>(decltype(msaOpt.sc)::TAB_SIZE), static_cast<size_t>(seqan::Blosum62::TAB_SIZE));
+
+    seqan::Blosum62 expected_matrix{};
+    for (size_t i = 0; i < static_cast<size_t>(seqan::Blosum62::TAB_SIZE); ++i)
+        EXPECT_EQ(msaOpt.sc.data_tab[i], expected_matrix.data_tab[i]);
+}
 
 TEST(align_multiple_test, the_first_dna4_test)
 {
@@ -87,7 +114,10 @@ TEST(align_multiple_test, the_first_aminoacid_test)
     copy(std::string_view{"--DPNKPKRAPSAFFVFMGEFREEFKQKNPKNKSVAAVGKAAGERWKSLSESEKAPYVAKANKLKGEYNKAIAAYNKGESA"}
          | seqan3::views::char_to<seqan3::gapped<seqan3::aa27>>, std::cpp20::back_inserter(output[3]));
 
-    auto result = seqan3::align_multiple(input);
+    seqan3::aminoacid_scoring_scheme scheme{seqan3::aminoacid_similarity_matrix::BLOSUM62};
+    seqan3::configuration config = seqan3::align_cfg::scoring{scheme};
+
+    auto result = seqan3::align_multiple(input, config);
 
     EXPECT_RANGE_EQ(output, result);
 }
